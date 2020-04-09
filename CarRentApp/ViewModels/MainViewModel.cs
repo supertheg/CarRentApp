5ed83669e711ae66361e3ca688fdcde7be63d93f
Паша Резниченко <supertheg@gmail.com>
2020-04-09 13:16:26 +0500

    extracted utils

--- CarRentApp/ViewModels/MainViewModel.cs
+++ CarRentApp/ViewModels/MainViewModel.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
@@ -84,26 +82,5 @@ namespace CarRentApp
         EngineFail,
         AirConditioningFail,
         RadioFail
-    }
-
-    public class EnumDisplayNameConverter : EnumConverter
-    {
-        public EnumDisplayNameConverter(Type type) : base(type) { }
-
-        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
-        {
-            if (destinationType == typeof(string)) {
-                if (value != null) {
-                    var fi = value.GetType().GetField(value.ToString());
-                    if (fi != null) {
-                        return (fi.GetCustomAttributes(typeof(DisplayAttribute), false).FirstOrDefault() is DisplayAttribute attribute) && (!string.IsNullOrEmpty(attribute.Name))
-                            ? attribute.Name
-                            : value.ToString();
-                    }
-                }
-                return string.Empty;
-            }
-            return base.ConvertTo(context, culture, value, destinationType);
-        }
     }
 }
---
+++ CarRentApp/ViewUtils/EnumDisplayNameConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace CarRentApp
+{
+    public class EnumDisplayNameConverter : EnumConverter
+    {
+        public EnumDisplayNameConverter(Type type) : base(type) { }
+
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof(string)) {
+                if (value != null) {
+                    var fi = value.GetType().GetField(value.ToString());
+                    if (fi != null) {
+                        return (fi.GetCustomAttributes(typeof(DisplayAttribute), false).FirstOrDefault() is DisplayAttribute attribute) && (!string.IsNullOrEmpty(attribute.Name))
+                            ? attribute.Name
+                            : value.ToString();
+                    }
+                }
+                return string.Empty;
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
